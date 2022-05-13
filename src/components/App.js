4cc8f7476ed78e894b1337702d0378750e2b65fb
jakechaley <jakechaley@gmail.com>
2022-05-13 09:34:37 -0700

    begin filling out basic file structure. header not showing for some reason.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { GameController } from "./GameController";
+import Header from "./Header";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header/>
+      <GameController/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Welcome To Black Jack! Feeling Lucky?</h1>
+    </React.Fragment>
+  );
+}
+
+export default Header;
