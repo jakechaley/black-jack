7ccbf413ee2379de753c4fd6e53f82bd8f3e0098
jakechaley <jakechaley@gmail.com>
2022-05-13 11:36:34 -0700

    work on building out a deck of cards

--- src/components/GameController.js
+++ src/components/GameController.js
@@ -4,7 +4,15 @@ class GameController extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      deck: [],
+      playerHand: [],
+      dealerHand: [],
+      playerScore: 0,
+      dealerScore: 0,
+      playerWin: false,
+      dealerWin: false,
+      hit: false,
+      finish: false
     };
   }
 }
---
+++ src/lib/DeackOfCards.js
@@ -0,0 +1,2 @@
+const suits = ["spades", "diamonds", "clubs", "hearts"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
